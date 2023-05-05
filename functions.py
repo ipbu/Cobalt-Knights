18889cd40413906ef4d770baf9ca1b90a2baf627
ipbu <silasmaxwelldutra+1@gmail.com>
2023-05-05 15:34:53 -0400

    Roadmap Update and Framework Update

--- functions.py
+++ functions.py
@@ -3,7 +3,20 @@ import time
 import colorama
 
 def ctxt(text, color):
-    pass
+    match color:
+        case "red":
+            color = colorama.Fore.RED
+        case "yellow":
+            color = colorama.Fore.YELLOW
+        case "green":
+            color = colorama.Fore.GREEN
+        case "cyan":
+            color = colorama.Fore.CYAN
+        case "blue":
+            color = colorama.Fore.BLUE
+        case "magenta":
+            color = colorama.Fore.MAGENTA
+    return color + text
 def tprint(text, time):
     time.sleep(time)
     print(text)
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from functions import *
+import colorama
+from replit import db
+colorama.init(autoreset=True)
